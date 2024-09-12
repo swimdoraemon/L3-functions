cfdb16b1e1d576b097a204f8d52e661709112d3d
swimdoraemon <indoaradhya@gmail.com>
2024-09-12 20:47:41 +0900

    calucalator

---
+++ calucalator.py
@@ -0,0 +1,27 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+
+
+num1 = int (input("Enter Number 1 :"))
+num2 = int (input("Enter Number 2 :"))
+
+print("sum :", add(num1,num2))
+print("subtract :", subtract(num1,num2))
+print("product :", multiply(num1,num2))
+print("quotient :", divide(num1,num2))
+
+
